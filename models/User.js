e8ae65cffb64d29aa95d5d4c4f3d244b830cdb05
Gowthambalaji <sbalajigowtham@gmail.com>
2025-11-02 10:40:29 +0530

    Your updated changes message

--- models/User.js
+++ models/User.js
@@ -1,10 +1,36 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
-const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  resetPasswordToken: { type: String },
-  resetPasswordExpire: { type: Date },
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      match: [/^\S+@\S+\.\S+$/, "Please enter a valid email"],
+    },
+    password: { type: String, required: true },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+  },
+  { timestamps: true }
+);
+
+// Hash password before save if modified
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
+
+// Remove sensitive fields from response
+userSchema.methods.toJSON = function () {
+  const user = this.toObject();
+  delete user.password;
+  delete user.resetPasswordToken;
+  delete user.resetPasswordExpire;
+  return user;
+};
 
 export default mongoose.model("User", userSchema);
